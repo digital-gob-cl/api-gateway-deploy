8047cace30f700c887afdae974f4fe5ab723cdcb
Marcelo Silva <msilva@arkho.io>
2023-04-17 12:01:10 -0400

    Correccion script

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,4 @@
-FROM --platform=linux/amd64 ubuntu:latest 
-
-RUN apt-get -y update 
-
-RUN apt-get -y install curl unzip jq git
-
-RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"; \
-    unzip awscliv2.zip ;\
-    ./aws/install -i /usr/local/aws-cli -b /usr/local/bin  --update
-
-RUN curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 ; \
-    chmod 700 get_helm.sh ;\
-    ./get_helm.sh
-
-
-RUN curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl ;\
-    chmod +x ./kubectl ;\
-    mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin ;\
-    echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc 
+FROM arkhotech/github-action-image:1.0
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,69 +1,70 @@
- #!/bin/bash
+#!/bin/bash
+set -e
 
-    if [ ! -z "$INPUT_ENVIRONMENT" ];
-    then 
-        ENVIRONMENT=$INPUT_ENVIRONMENT
-    fi
+if [ ! -z "$INPUT_ENVIRONMENT" ];
+then 
+    ENVIRONMENT=$INPUT_ENVIRONMENT
+fi
 
-    PROJECT=$INPUT_PROJECT
+PROJECT=$INPUT_PROJECT
 
-    echo "Revisando balancers"
-    NLB_LIST=$(aws elbv2 describe-load-balancers | jq -r ' [  .LoadBalancers[] | select( .Type=="network" ) |   { arn: .LoadBalancerArn, hostname: .DNSName } ] ')
-    
-    HOSTNAME=$(kubectl get services -l cpat.service=$SERVICE_NAME -n cpat -o json | jq -r ' .items[].status.loadBalancer.ingress[].hostname')
+echo "Revisando balancers"
+NLB_LIST=$(aws elbv2 describe-load-balancers | jq -r ' [  .LoadBalancers[] | select( .Type=="network" ) |   { arn: .LoadBalancerArn, hostname: .DNSName } ] ')
 
-    echo "Buscando NLB: $HOSTNAME"
-    
-    ARN=$(echo $NLB_LIST | jq --arg h $HOSTNAME -r '.[] | select( .hostname == $h ) | .arn' )
-   
-    #Listar los links que estan desplegados
-    NLBS=$(aws apigateway get-vpc-links | jq -r ' [ .items[].targetArns[] ] ')
-    
-    RES=$(echo $NLBS | jq --arg t $ARN 'select( . | index($t) )')
-    echo "Resultado de la busqueda: $RES"
-    
-    if [ -z "$RES" ];
-    then
-        echo "No existe el VPC Link"
-        echo "$PROJECT-$SERVICE_NAME-$ENVIRONMENT-link"
+HOSTNAME=$(kubectl get services -l cpat.service=$SERVICE_NAME -n cpat -o json | jq -r ' .items[].status.loadBalancer.ingress[].hostname')
 
-        aws apigateway create-vpc-link \
-            --name "$PROJECT-$SERVICE_NAME-$ENVIRONMENT-link" \
-            --description "Link para API servicio $SERVICE_NAME en ambiente $ENVIRONMENT" \
-            --target-arns "$ARN" \
-            --tags "Environment=$ENVIRONMENT,Project=$PROJECT,Purpose=API"
-    else
-        echo "Ya existe NLB para el servicio $SERVICE_NAME"
-    fi
-    #Actualizando API
+echo "Buscando NLB: $HOSTNAME"
 
-    API_NAME="$PROJECT-$ENVIRONMENT-$SERVICE_NAME-api"
+ARN=$(echo $NLB_LIST | jq --arg h $HOSTNAME -r '.[] | select( .hostname == $h ) | .arn' )
 
-    #Check si existe api gateway 
-    API_DATA=$(aws apigateway get-rest-apis | jq -r --arg n $API_NAME ' .items[] | select( .name == $n)')
+#Listar los links que estan desplegados
+NLBS=$(aws apigateway get-vpc-links | jq -r ' [ .items[].targetArns[] ] ')
+
+RES=$(echo $NLBS | jq --arg t $ARN 'select( . | index($t) )')
+echo "Resultado de la busqueda: $RES"
+
+if [ -z "$RES" ];
+then
+    echo "No existe el VPC Link"
+    echo "$PROJECT-$SERVICE_NAME-$ENVIRONMENT-link"
+
+    aws apigateway create-vpc-link \
+        --name "$PROJECT-$SERVICE_NAME-$ENVIRONMENT-link" \
+        --description "Link para API servicio $SERVICE_NAME en ambiente $ENVIRONMENT" \
+        --target-arns "$ARN" \
+        --tags "Environment=$ENVIRONMENT,Project=$PROJECT,Purpose=API"
+else
+    echo "Ya existe NLB para el servicio $SERVICE_NAME"
+fi
+#Actualizando API
+
+API_NAME="$PROJECT-$ENVIRONMENT-$SERVICE_NAME-api"
+
+#Check si existe api gateway 
+API_DATA=$(aws apigateway get-rest-apis | jq -r --arg n $API_NAME ' .items[] | select( .name == $n)')
+
+ID=$(echo "$API_DATA" | jq -r '.id')
+
+if [ -z ${API_DATA} ];
+then
+    echo "Creando API Gateway"
+    API_DATA=$(aws apigateway create-rest-api --name=$API_NAME \
+                        --endpoint-configuration "types=REGIONAL" \
+                        --description "API Gateway servicio $SERVICE_NAME")
 
     ID=$(echo "$API_DATA" | jq -r '.id')
 
-    if [ -z ${API_DATA} ];
-    then
-        echo "Creando API Gateway"
-        API_DATA=$(aws apigateway create-rest-api --name=$API_NAME \
-                            --endpoint-configuration "types=REGIONAL" \
-                            --description "API Gateway servicio $SERVICE_NAME")
+fi
 
-        ID=$(echo "$API_DATA" | jq -r '.id')
+if [[ "$OSTYPE" == "linux-gnu"* ]];
+then
+    echo "$OSTYPE"
+    base64 ./${INPUT_SWAGGER_PATH} > ./swager_body.b64
+else
+    base64 -i ./${INPUT_SWAGGER_PATH} -o ./swager_body.b64
+fi 
+    
+echo "Actualizando API $ID"
 
-    fi
-
-    if [[ "$OSTYPE" == "linux-gnu"* ]];
-    then
-        echo "$OSTYPE"
-        base64 ./${INPUT_SWAGGER_PATH} > ./swager_body.b64
-    else
-        base64 -i ./${INPUT_SWAGGER_PATH} -o ./swager_body.b64
-    fi 
-        
-    echo "Actualizando API $ID"
-
-    aws apigateway put-rest-api --rest-api-id $ID \
-            --body file://./swager_body.b64 
+aws apigateway put-rest-api --rest-api-id $ID \
+        --body file://./swager_body.b64 
