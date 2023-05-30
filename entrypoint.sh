a170dd640a7e9c59049b2b8005c6e628ede302de
Marcelo Silva <msilva@arkho.io>
2023-05-30 17:00:48 -0400

    Correccion

--- entrypoint.sh
+++ entrypoint.sh
@@ -95,7 +95,7 @@ sed -i 's/ACCOUNT_ID/"'$ACCOUNT_ID'"/g'  ./swagger_temp.yaml
 
 sed -i 's/REGION/"'$AWS_DEFAULT_REGION'"/g'  ./swagger_temp.yaml
 
-
+cat  ./swagger_temp.yaml
 
 if [[ "$OSTYPE" == "linux-gnu"* ]];
 then
